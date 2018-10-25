45904eb5859a6fad5b4967b2f59d17b404b8f4ad
soeon Kim <developeon80@gmail.com>
2018-10-25 10:48:40 +0900

    Verify 기능 구현

--- server/routes/api/siginin.js
+++ server/routes/api/siginin.js
@@ -138,5 +138,32 @@ module.exports = (app) => {
             })
         });
     });
+
+    app.get('/api/account/verify', (req, res, next) => {
+        const { query } = req;
+        const { token } = query;
+        UserSession.find({
+            _id: token,
+            isDeleted: false
+        }, (err, sessions) => {
+            if(err){
+                return res.send({
+                    success: false,
+                    message: 'Error: Server erorr'
+                });
+            }
+            if(sessions.length != 1){
+                return res.send({
+                    success: false,
+                    message: 'Error: Invalid'
+                });
+            } else{
+                return res.send({
+                    success: true,
+                    message: 'Success: Good'
+                });
+            }
+        });
+    });
 };
   
